094fa58ee19fad37ee43d05bea1bf9d137831969
ChangHun Lee <loslch@gmail.com>
2014-06-24 18:48:28 +0900

    JSON 모듈 추가 및 static 폴더 생성

--- src/main/java/com/nhnent/rookie/NHNGuestBook/controller/HelloWorldController.java
+++ src/main/java/com/nhnent/rookie/NHNGuestBook/controller/HelloWorldController.java
@@ -1,16 +1,21 @@
 package com.nhnent.rookie.NHNGuestBook.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloWorldController { 
+public class HelloWorldController {
 
-	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
     public String hello(HttpServletRequest req, HttpServletResponse res) {
 		String name = req.getParameter("name");
 		req.setAttribute("name", name);
@@ -18,4 +23,14 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+	@RequestMapping(value = "/json", method=RequestMethod.GET)
+	public String login(HttpServletRequest req, HttpServletResponse res) {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("status", "success");
+		result.put("message", "You have accessed servlet page.");
+
+		JSONObject jsonResult = JSONObject.fromObject(result);
+		req.setAttribute("output", jsonResult);
+		return "output";
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,8 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
+<head>
+  <script src='<c:url value="/static/js/jquery-1.11.1.min.js" />'></script>
+</head>
 <body>
 <h2>Hello World!</h2>
 </body>
