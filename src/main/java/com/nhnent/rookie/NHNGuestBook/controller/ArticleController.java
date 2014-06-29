406e0cbb1b92a2bac4990474907d7d5af70a92e9
ChangHun Lee <loslch@gmail.com>
2014-06-29 11:16:07 +0900

    Connect Cubrid database and implement get articles

--- src/main/java/com/nhnent/rookie/NHNGuestBook/controller/ArticleController.java
+++ src/main/java/com/nhnent/rookie/NHNGuestBook/controller/ArticleController.java
@@ -1,37 +1,83 @@
 package com.nhnent.rookie.NHNGuestBook.controller;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sf.json.JSONObject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nhnent.rookie.NHNGuestBook.model.Article;
+import com.nhnent.rookie.NHNGuestBook.model.ResponseMessage;
 
 @Controller
-public class GuestBookController {
+@RequestMapping(value = "/article")
+public class ArticleController {
+    Statement stmt = null;
+    ResultSet rs = null;
+    Connection conn = null;
+    
+	@RequestMapping(method=RequestMethod.GET)
+	public @ResponseBody ResponseMessage getArticles() {
+		ResponseMessage msg = new ResponseMessage();
+    	List<Article> articles = new ArrayList<Article>();
+    			
+	    try {
+	        Context initCtx = new InitialContext();
+	        Context envCtx = (Context)initCtx.lookup("java:/comp/env");
+	        DataSource ds = (DataSource)envCtx.lookup("jdbc/CUBRIDDS");        
+	        conn = ds.getConnection();
+	        
+	        String sQuery = "select [no], email, body, regtime, modtime from articles";
+	        stmt = conn.createStatement();
+	        rs = stmt.executeQuery(sQuery);
+	        
+			while (rs.next()) {
+				Article article = new Article();
+				article.setNo(rs.getLong(1));
+				article.setEmail(rs.getString(2));
+				article.setBody(rs.getString(3));
+				article.setRegtime(rs.getTimestamp(4));
+				article.setModtime(rs.getTimestamp(5));
+				articles.add(article);
+			}
 
-	@RequestMapping(value = "/json", method=RequestMethod.GET)
-	public String json(HttpServletRequest req, HttpServletResponse res) {
-		Map<String, String> result = new HashMap<String, String>();
-		result.put("status", "success");
-		result.put("message", "You have accessed servlet page.");
+			msg.setStatus("success");
+			msg.setArticles(articles);
+	    } catch(Exception ex) {
+	        ex.printStackTrace();
+			msg.setStatus("fail");
+			msg.setMessage("Something went wrong!");
+	    } finally {
+	        if (rs != null) try {rs.close(); } catch (Exception ex2) {}
+	        if (stmt != null) try {stmt.close(); } catch (Exception ex3) {}
+	        if (conn != null) try {conn.close(); } catch (Exception ex4) {}
+	    }
+		
+		return msg;
+	}
 
-		JSONObject jsonResult = JSONObject.fromObject(result);
-		req.setAttribute("output", jsonResult);
-		res.setContentType("application/json; charset=UTF-8");
-		try {
-			res.getWriter().write(jsonResult.toString());
-			res.flushBuffer();
-			res.setStatus(HttpServletResponse.SC_OK);
-		} catch (IOException e) {
-			res.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-		}
+	@RequestMapping(method=RequestMethod.POST)
+	public @ResponseBody ResponseMessage postArticle(@RequestBody final Article article) {
+		ResponseMessage msg = new ResponseMessage();
+		msg.setStatus("success");
+		msg.setMessage("Your Message has been successfully submitted.");
+		
+		return msg;
+	}
+
+	@RequestMapping(value="/{no}", method=RequestMethod.GET)
+	public String getArticle(@PathVariable int name) {
 		return null;
 	}
 }
---
+++ src/main/java/com/nhnent/rookie/NHNGuestBook/model/ResponseMessage.java
@@ -0,0 +1,33 @@
+package com.nhnent.rookie.NHNGuestBook.model;
+
+import java.util.List;
+
+public class ResponseMessage {
+	private String status;
+	private String message;
+	private List<Article> articles;
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+}
